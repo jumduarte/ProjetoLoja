6f93f7978ce553243d14d05ce7300a407c20892b
jumduarte <juliamaria.duarte12@gmail.com>
2023-11-21 16:31:06 -0300

    project

--- src/app/sessao/sessao.component.ts
+++ src/app/sessao/sessao.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import Swal from 'sweetalert2';
+import { OnInit } from '@angular/core';
+import { SessaoService } from './service/sessao.service';
+import { Isessao } from './service/isessao';
 
 @Component({
   selector: 'app-sessao',
   templateUrl: './sessao.component.html',
   styleUrls: ['./sessao.component.scss']
 })
-export class SessaoComponent {
+export class SessaoComponent implements OnInit {
+  ngOnInit(): void { this.listar() }
+
+  produtos: Isessao[] = [];
+
+  constructor(private service: SessaoService){}
+
+  listar(){
+    this.service.listar().subscribe(dados => this.produtos = dados);
+  
+  }
   Comprar(){
     Swal.fire({
       title: "Você deseja comprar esse produto?",
@@ -22,5 +35,4 @@ export class SessaoComponent {
       }
     });
   }
-
 }
